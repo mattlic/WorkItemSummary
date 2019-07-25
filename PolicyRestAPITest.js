4d4c171cbbf4e4a88d001589021c6964004f01a8
Matt Licholai <mattlic@outlook.com>
2019-07-25 02:06:09 -0700

    added policies test

--- PolicyRestAPITest.js
+++ PolicyRestAPITest.js
@@ -1,18 +1,20 @@
 var unirest = require("unirest"); 
  
 var baseURL = "https://dev.azure.com"
-var restApiStr = "_apis/wit"
-var restApiAction = "queries"
+var restApiStr = "_apis/policy"
+var restApiAction = "configurations"
 var azdoQueryId = '8edae917-512a-491b-89b5-3ba761394e46'
-var queryStr= "api-version=5.1-preview.2"
+var queryStr= "api-version=5.1"
 var queryStr2= "$depth=1"
 var organization = "DevDiv";
 var project = "DevDiv";
 const username = "mattlic@microsoft.com";
 const pat = "ny57ltjnw6oku5gwel6g3pnfifcdamwpj5y64npu7fpolis6tnga";
 
-useURL = baseURL + "/" + organization + "/" + project + "/" + restApiStr + "/" + restApiAction + "/" 
-useURL += azdoQueryId
+var policies
+
+var useURL = baseURL + "/" + organization + "/" + project + "/" + restApiStr + "/" + restApiAction 
+// useURL += ("/" + azdoQueryId)
 console.log("URL to send: " + useURL + " \n")
 // var req = unirest("GET", "https://dev.azure.com/{organization}/{project}/_apis/wit/queries?api-version=5.1-preview.2")
 // this is the URL to try
@@ -21,7 +23,7 @@ console.log("URL to send: " + useURL + " \n")
 var req = unirest("GET", useURL)
 
 // var auth = 'Basic ' + Buffer.from(username + ':' + pat).toString('base64');
-var authStr = 'Basic' + (" :" + pat).toString('base64');  // don't include username with a PAT
+// var authStr = 'Basic' + (" :" + pat).toString('base64');  // don't include username with a PAT
 
 req.auth({
     user: "",
@@ -29,7 +31,7 @@ req.auth({
     sendImmediately: true
 })
 
-req.query( queryStr2 )
+// req.query( queryStr2 )
 req.query( queryStr )
 
 req.end(function (res) {
@@ -57,5 +59,9 @@ req.end(function (res) {
         console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
         console.log("\n\nResponse:\n")
         if (res.error) console.warn(res.error)
-        else console.log(res.body)
+        else {
+            // console.log(res.body)
+            policies = res.body.value
+            console.log(policies[1])
+        }
 })
