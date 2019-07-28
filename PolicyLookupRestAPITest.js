c87a3d9258ee3145f1b85b2608af6bd72d6cd0a1
Matt Licholai <mattlic@outlook.com>
2019-07-28 11:48:24 -0700

    Getting the lookups to work correctly

--- PolicyLookupRestAPITest.js
+++ PolicyLookupRestAPITest.js
@@ -34,35 +34,6 @@ req.auth({
 // req.query( queryStr2 )
 req.query(queryStr)
 
-function lookupNameReq(userID) {
-    var baseURL = "https://vssps.dev.azure.com"
-    var restApiStr = "_apis/graph"
-    var restApiAction = "users"
-    var queryStr = "api-version=5.1"
-    var useURL = baseURL + "/" + organization + "/" + restApiStr + "/" + restApiAction
-    useURL += userID
-    var req = unirest("GET", useURL)
-    req.query(queryStr)
-    req.auth({
-        user: "",
-        pass: pat,
-        sendImmediately: true
-    });
-    return req;
-}
-
-function getNameFromId(userID) {
-    lookupNameReq(userID).end(function (res) {
-            var reply = res.body;
-            console.log( 'userID is: ' + userID);
-            console.log("lookup result is: " + reply );
-            var replyResult = JSON.parse( reply );
-
-        }
-    );
-}
-
-
 req.end(function (res) {
     //if (res.error) throw new Error(res.error)
     var respStatus = "unknown"
@@ -83,29 +54,26 @@ req.end(function (res) {
             respStatus = "Server Error"
             break
     }
-    console.log("\n\nPolicy API Returned:\nResponse code: " + res.code)
-    console.log("  status type: " + respStatus)
-    console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
     console.log("\n\nResponse:\n")
     if (res.error) {
+        console.log("\n\nPolicy API Returned:\nResponse code: " + res.code)
+        console.log("  status type: " + respStatus)
+        console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
         console.warn(res.error)
     } else {
         // console.log(res.body)
         // policies = JSON.parse(res.body);
         console.log("count is: " + res.body.count);
         var policies = res.body.value;
-        console.log("first policy has ID: " + policies[0]["id"]);
-        console.log("    and the type is: " + policies[0]["type"]["displayName"]);
-        console.log("    and the reviewer ID are: " + policies[0]["settings"]["requiredReviewerIds"]);
-        console.log("    and the reviewer names are: ");
-        var userID = policies[0]["settings"]["requiredReviewerIds"][0] 
-            console.log('       calling with userID: ' + userID );
-            console.log('          ' + getNameFromId(userID));
-        
+
         console.log("\nNow processing the policies ..... \n")
         var interstingPolicies = getPoliciesByType("Required reviewers", policies);
         // console.log( JSON.stringify( interstingPolicies, null, '\t' ));
         console.log("  Number of Required Reviewer policies: " + Object.keys(interstingPolicies).length);
+
+        console.log("Second reviewer policy has ID: " + interstingPolicies[2]["id"]);
+        console.log("    and the type is: " + interstingPolicies[2]["type"]["displayName"]);
+        console.log("    and the reviewers are: " + getReviewerInfo(interstingPolicies[2]));
 
     }
 })
@@ -118,3 +86,150 @@ function getPoliciesByType(type, policies) {
         }
     );
 }
+
+
+function getReviewerInfo(reviewerPolicy) {
+    console.log('  policy ID: ' + reviewerPolicy.id);
+    console.log('  requiredReviewerIds: ' + reviewerPolicy["settings"]["requiredReviewerIds"]);
+    var reviewerData = [];
+    var name;
+
+    reviewerPolicy["settings"]["requiredReviewerIds"].forEach(function (reviewer) {
+        name = getNameFromId(reviewer);
+        console.log('  composing return value with -   name: ' + name + ', id: ' + reviewer);
+        reviewerData.concat({
+            '    name': name,
+            '    id': reviewer
+        });
+    });
+    return reviewerData;
+}
+
+function getNameFromId(userID) {
+    var descriptor = lookupIdStorageUser( lookupStorageIDReq( userID ) );
+    var name = lookupDescriptorNameuser( lookupDescriptorNameReq( descriptor ) );
+    console.log( '   userID: ' + userID + ' , Descriptor: ' + descriptor + ' , Name: ' + name );
+    return name;
+}
+
+
+function lookupIdStorageUser(userIdReq) {
+    lookupDescriptorNameReq(userIdReq).end(function (res) {
+        var reply = res.body;
+        var respStatus = "unknown"
+        switch (res.statusType) {
+            case 1:
+                respStatus = "Info"
+                break
+            case 2:
+                respStatus = "Ok"
+                break
+            case 3:
+                respStatus = "Miscellaneous"
+                break
+            case 4:
+                respStatus = "Client Error"
+                break
+            case 5:
+                respStatus = "Server Error"
+                break
+        }
+        if (res.error) {
+            console.log("\n\nGetName API Returned:\nResponse code: " + res.code)
+            console.log("  status type: " + respStatus)
+            console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
+            console.log("\n\nResponse:\n")
+            console.warn(res.error)
+        } else {
+            // console.log('user Descriptor is: ' + userID);
+            console.log("lookup result is: " + reply);
+            // var replyResult = JSON.parse( reply );
+            var descriptor = reply.value;
+            console.log('   Descriptor: ' + descriptor);
+            return descriptor;
+        }
+    });
+}
+
+function lookupDescriptorNameuser(userDescriptorReq) {
+    lookupDescriptorNameReq(userDescriptorReq).end(function (res) {
+        var reply = res.body;
+        var respStatus = "unknown"
+        switch (res.statusType) {
+            case 1:
+                respStatus = "Info"
+                break
+            case 2:
+                respStatus = "Ok"
+                break
+            case 3:
+                respStatus = "Miscellaneous"
+                break
+            case 4:
+                respStatus = "Client Error"
+                break
+            case 5:
+                respStatus = "Server Error"
+                break
+        }
+        if (res.error) {
+            console.log("\n\nGetName API Returned:\nResponse code: " + res.code)
+            console.log("  status type: " + respStatus)
+            console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
+            console.log("\n\nResponse:\n")
+            console.warn(res.error)
+        } else {
+            // console.log('user Descriptor is: ' + userID);
+            console.log("lookup result is: " + reply);
+            // var replyResult = JSON.parse( reply );
+            var name = reply.displayName;
+            var email = reply.mailAddress;
+            console.log('  name: ' + name + ', email: ' + email);
+            return name;
+        }
+    });
+}
+
+function lookupDescriptorNameReq(userDescriptor) {
+    var baseURL = "https://vssps.dev.azure.com"
+    var restApiStr = "_apis/graph"
+    var restApiAction = "users"
+    var queryStr = "api-version=5.1"
+    var useURL = baseURL + "/" + organization + "/" + restApiStr + "/" + restApiAction + "/" + userDescriptor
+    var req = unirest("GET", useURL)
+    req.query(queryStr)
+    req.auth({
+        user: "",
+        pass: pat,
+        sendImmediately: true
+    });
+    return req;
+}
+
+function lookupStorageIDReq(userID) {
+    var baseURL = "https://vssps.dev.azure.com"
+    var restApiStr = "_apis/graph"
+    var restApiAction = "descriptors"
+    var queryStr = "api-version=5.1"
+    var useURL = baseURL + "/" + organization + "/" + restApiStr + "/" + restApiAction + "/" + userID
+    var req = unirest("GET", useURL)
+    req.query(queryStr)
+    req.auth({
+        user: "",
+        pass: pat,
+        sendImmediately: true
+    });
+    return req;
+}
+
+
+
+
+
+/*
+    console.log("    and the reviewer names are: ");
+        var userID = interstingPolicies[1]["settings"]["requiredReviewerIds"][0] 
+            console.log('       calling with userID: ' + userID );
+            console.log('          ' + getNameFromId(userID));
+        
+*/
