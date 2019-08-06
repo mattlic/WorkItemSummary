402c27e480839514724e5b03c327e532afe44d3b
Matt Licholai <mattlic@outlook.com>
2019-08-05 22:44:41 -0700

    updated prototype

--- policyLookupPilot.js
+++ policyLookupPilot.js
@@ -19,23 +19,6 @@ var useURL = baseURL + "/" + organization + "/" + project + "/" + restApiStr + "
 console.log("URL to send: " + useURL + " \n")
 
 
-// var req = unirest("GET", "https://dev.azure.com/{organization}/{project}/_apis/wit/queries?api-version=5.1-preview.2")
-// this is the URL to try
-// var urlTest = "https://dev.azure.com/DevDiv/DevDiv/_apis/wit/queries?api-version=5.1-preview.2"
-
-// var req = unirest("GET", useURL)
-
-// var auth = 'Basic ' + Buffer.from(username + ':' + pat).toString('base64');
-// var authStr = 'Basic' + (" :" + pat).toString('base64');  // don't include username with a PAT
-/*
-
-req.auth({
-    user: "",
-    pass: pat,
-    sendImmediately: true
-})
-*/
-
 unirest
     .get(useURL)
     .query(queryStr)
@@ -44,17 +27,33 @@ unirest
         pass: pat,
         sendImmediately: true
     }).then((response) => {
-        console.log( getReturnCode( response) );
-        return (response.body);
-    }).then((response) => {
-        // console.log(response);
-        console.log("count is: " + response.count);
-        var policies = response.value;
+        const returnStatus = getReturnCode(response);
+        if (returnStatus == 'Ok') {
+            console.log("count is: " + response.body.count);
+            var policies = response.body.value;
+            var reviewerPolicies = getPoliciesByType("Required reviewers", policies);
+            console.log("  Number of Required Reviewer policies: " + Object.keys(reviewerPolicies).length);
+            // console.log( reviewerPolicies[1]);
+            var policySummary = [];
+            for (var policy in reviewerPolicies) {
+                policySummary.concat({
+                    settings: policy.settings,
+                    reviewerIds: policy.settings.requiredReviewerIds
+                });
+            }
+            console.log(policySummary[1]);
+        } else {
+            console.log('Did not get an Ok response');
+            console.log('  Ruturn status was: ' + returnStatus);
+            console.log("  Originalrequest:\n" + JSON.stringify(response.request, null, '\t'));
+            throw new Error('did not get an Ok response, Status: ' + returnStatus);
+        }
     }).catch((error) => {
-        console.warn(error)
+        console.warn(error);
     });
 
-const getReturnCode = function ( res ) {
+
+const getReturnCode = function (res) {
     var respStatus = "unknown"
     switch (res.statusType) {
         case 1:
@@ -75,56 +74,6 @@ const getReturnCode = function ( res ) {
     }
     return respStatus
 }
-
-
-
-
-/*
-req.end(function (res) {
-    //if (res.error) throw new Error(res.error)
-    var respStatus = "unknown"
-    switch (res.statusType) {
-        case 1:
-            respStatus = "Info"
-            break
-        case 2:
-            respStatus = "Ok"
-            break
-        case 3:
-            respStatus = "Miscellaneous"
-            break
-        case 4:
-            respStatus = "Client Error"
-            break
-        case 5:
-            respStatus = "Server Error"
-            break
-    }
-    console.log("\n\nResponse:\n")
-    if (res.error) {
-        console.log("\n\nPolicy API Returned:\nResponse code: " + res.code)
-        console.log("  status type: " + respStatus)
-        console.log("Originalrequest:\n" + JSON.stringify(res.request, null, '\t'))
-        console.warn(res.error)
-    } else {
-        // console.log(res.body)
-        // policies = JSON.parse(res.body);
-        console.log("count is: " + res.body.count);
-        var policies = res.body.value;
-
-        console.log("\nNow processing the policies ..... \n")
-        var interstingPolicies = getPoliciesByType("Required reviewers", policies);
-        // console.log( JSON.stringify( interstingPolicies, null, '\t' ));
-        console.log("  Number of Required Reviewer policies: " + Object.keys(interstingPolicies).length);
-
-        console.log("Second reviewer policy has ID: " + interstingPolicies[2]["id"]);
-        console.log("    and the type is: " + interstingPolicies[2]["type"]["displayName"]);
-        console.log("    and the reviewers are: " + getReviewerInfo(interstingPolicies[2]));
-
-    }
-})
-
-*/
 
 
 function getPoliciesByType(type, policies) {
